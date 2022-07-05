ec18449324f7cb75f8d89b0976641bac0c25f7a3
rnll <neomashab@gmail.com>
2022-07-06 00:07:13 +0530

    Clean up of init.el

--- init.el
+++ init.el
@@ -2,13 +2,6 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(which-key doom-modeline all-the-icons doom-themes projectile evil-collection evil dashboard general use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
